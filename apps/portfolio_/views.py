edf9976440d93cdd73b49be23c1a2daf1e7f172f
Michael <vmichaelkirkaldy@live.com>
2019-05-07 12:33:47 -0500

    imported stmplib for email

--- apps/portfolio_/views.py
+++ apps/portfolio_/views.py
@@ -22,4 +22,21 @@ def resume(request):
 	return render(request, 'portfolio_/resume.html')
 
 def form_process(request):
-	return redirect()
+	#context manager makes sure connection is closed without needing to do manually.
+	#mail server/ port no.
+	print request
+	server = smtplib.SMTP('smtp.gmail.com',587)
+	server.ehlo()
+	server.starttls()
+	server.ehlo()
+
+	server.login('kirkaldymichaelv@gmail.com', 'afszipzhfurcupei')
+
+	subject = request.POST['subject']
+	body = request.POST['body']
+	client_email = request.POST['email']
+
+	msg = 'subject: {}\n\n{}'.format(subject, body)
+
+	server.sendmail(client_email, 'kirkaldymichaelv@gmail.com', msg)
+	return redirect('/')
